dcd92da4e0070bb3e6f28028f3dfce23ec5a6439
natoliveira <nataliadeolive@gmail.com>
2022-06-23 21:23:23 -0300

    codigos

--- FibonacciRecursivo.cpp
+++ FibonacciRecursivo.cpp
@@ -1,29 +1,25 @@
+//sem programação dinâmica
 #include <iostream>
-//algoritmo fibonacci Recursivo
-//o custo do algoritmo é dado pela relação de recorência: t(n) = c(n-1) + c(n-2)
-// a ordem de complexidade deste algoritmo é 0(n)
+#include <vector>
+
 using namespace std;
 
-int FibonacciR( int n, int N)
-{
+int fibonacci(int n){
 
-    if(n==0 || n==1)
-    {
-        return n;
-    }
-    if(n >= -1)
-    {
-             N = FibonacciR(n-1) + FibonacciR(n-2);
-    }
-    return N;
-
+if (n >= 1)
+    
+    return n;
+    
+    else 
+    
+    fibonacci(n-1) + fibonacci(n-2);
+    
+    return 0;
 }
 
-
-int main()
-{
-
-int n = 3;
-cout << FibonacciR(n, N) << "\n";
-
+int main(){
+    int n;
+    cout << "qual posição?" << endl;
+    cin >>n;
+    cout << fibonacci(n) << endl;
 }
--- fibonacciDinamico.cpp
+++ fibonacciDinamico.cpp
@@ -1,11 +1,9 @@
 #include <vector>
 #include <iostream>
-//algoritmo fibonacci Dinamico
-//o custo do algoritmo é dado pela relação de recorência: t(n) = c(n-1) + c(n-2)
-// a ordem de complexidade deste algoritmo é 0(n)
+
 using namespace std;
 
-int FibonacciD(vector<int> &v, int n)
+int FibonacciDP(vector<int> &v, int n)
 {
 
     if(n==0 || n==1)
@@ -14,7 +12,7 @@ int FibonacciD(vector<int> &v, int n)
     }
     if(v[n - 1] >= -1)
     {
-             v[n-1] = FibonacciD(v, n-1) + FibonacciD(v, n-2);
+             v[n-1] = FibonacciDP(v, n-1) + FibonacciDP(v, n-2);
     }
     return v[n - 1];
 
@@ -24,9 +22,9 @@ int FibonacciD(vector<int> &v, int n)
 int main()
 {
 
-int n = 3;
+int n = 10;
 vector < int > v(n,-1);
 
-cout << FibonacciD(v, n) << "\n";
+cout << FibonacciDP(v, n) << "\n";
 
 }
